9cc74a332d58c53aacf43a144ec0caf9a755a0ee
xwf <420842659@qq.com>
2020-08-05 22:29:59 +0800

    Bean Factory 生命周期

---
+++ application-context-lifecycle/src/main/java/com/huajie/thinking/in/spring/application/context/lifecycle/LiveBeansViewDemo.java
@@ -0,0 +1,11 @@
+package com.huajie.thinking.in.spring.application.context.lifecycle;
+
+/**
+ * @author ：xwf
+ * @date ：Created in 2020\8\5 0005 22:29
+ */
+public class LiveBeansViewDemo {
+    public static void main(String[] args) {
+
+    }
+}
